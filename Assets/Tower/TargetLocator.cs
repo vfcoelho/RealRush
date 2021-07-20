080dbde6bffb6e5611c469d38c2f2388604a4feb
vfcoelho <vfcoelho18.2@hotmail.com>
2021-07-20 04:25:29 -0300

    adds target closes enemy logic and range limitations

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticle;
+    [SerializeField] float range = 15f;//roughly enough world units to cover adjacent tiles 
     Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
 
-    void AimWeapon(){
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+                target = closestTarget;
+            }
+        }
+    }
+    void AimWeapon()
+    {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+        if (targetDistance < range){
+            Attack(true);
+        }else{
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
