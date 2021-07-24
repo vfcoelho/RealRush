5a45bce78fa11c41506550e33b9f54f7ef0d0a5c
vfcoelho <vfcoelho18.2@hotmail.com>
2021-07-24 19:42:30 -0300

    finishes breadth first algorithm

--- Assets/Pathfinding/Pathfinder.cs
+++ Assets/Pathfinding/Pathfinder.cs
@@ -12,7 +12,7 @@ public class Pathfinder : MonoBehaviour
     Node currentSearchNode;
 
     Queue<Node> frontier = new Queue<Node>();
-    Dictionary<Vector2Int,Node> reached = new Dictionary<Vector2Int, Node>();
+    Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
 
 
     Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
@@ -26,14 +26,15 @@ public class Pathfinder : MonoBehaviour
         {
             grid = gridManager.Grid;
         }
-
-        startNode = new Node(startCoordinates, true);
-        destinationNode = new Node(destinationCoordinates, true);
     }
     // Start is called before the first frame update
     void Start()
     {
+        startNode = gridManager.Grid[startCoordinates];
+        destinationNode = gridManager.Grid[destinationCoordinates];
+
         BreadthFirstSearch();
+        BuildPath();
     }
     void ExploreNeighbors()
     {
@@ -46,27 +47,51 @@ public class Pathfinder : MonoBehaviour
                 neighbors.Add(grid[neighborCoords]);
             }
         }
-        foreach(Node neighbor in neighbors){
-            if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable){
-                reached.Add(neighbor.coordinates,neighbor);
+        foreach (Node neighbor in neighbors)
+        {
+            if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+            {
+                neighbor.connectedTo = currentSearchNode;
+                reached.Add(neighbor.coordinates, neighbor);
                 frontier.Enqueue(neighbor);
             }
         }
     }
 
-    void BreadthFirstSearch(){
+    void BreadthFirstSearch()
+    {
         bool isRunning = true;
 
         frontier.Enqueue(startNode);
-        reached.Add(startNode.coordinates,startNode);
+        reached.Add(startNode.coordinates, startNode);
 
-        while(frontier.Count > 0 && isRunning){
+        while (frontier.Count > 0 && isRunning)
+        {
             currentSearchNode = frontier.Dequeue();
             currentSearchNode.isExplored = true;
             ExploreNeighbors();
-            if(currentSearchNode.coordinates == destinationCoordinates){
+            if (currentSearchNode.coordinates == destinationCoordinates)
+            {
                 isRunning = false;
             }
         }
+    }
+
+    List<Node> BuildPath(){
+        List<Node> path = new List<Node>();
+        Node currentNode = destinationNode;
+
+        path.Add(currentNode);
+        currentNode.isPath = true;
+
+        while(currentNode.connectedTo != null){
+            currentNode = currentNode.connectedTo;
+            path.Add(currentNode);
+            currentNode.isPath = true;
+        }
+
+        path.Reverse();
+
+        return path;
     }
 }
